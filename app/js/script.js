aa673c72a7cb09ba6697d052c7ede03ea51e9f45
majid-babaei <majid.1998babaei@gmail.com>
2024-09-15 20:40:22 +0330

    desktop project compeleted

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
-let goToTopButton = document.querySelector("#goToTop")
+let goToTopButton = document.querySelector("#goToTop");
 
-goToTopButton.addEventListener("click",(e)=>{
-     e.preventDefault();
-     window.scrollTo({top: 0, behavior: 'smooth'});
+goToTopButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
